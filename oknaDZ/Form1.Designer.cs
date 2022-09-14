8a47ee0363d19dde9999ada825e90b2a6213e576
kotru <kotru@DESKTOP-28GH3L5>
2022-09-14 11:35:24 +0300

    Добавьте файлы проекта.

---
+++ oknaDZ/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace oknaDZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(28, 29);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(269, 38);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Простое окно сообщения";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(28, 82);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(269, 38);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Затейливое окно сообщения";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(28, 140);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(269, 37);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Предупреждающее сообщение";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(28, 196);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(269, 36);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Удаление файла";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 270);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Окна сообщений";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
---
+++ oknaDZ/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oknaDZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DialogResult res;
+ 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            res = MessageBox.Show("Это простое окно сообщения","", MessageBoxButtons.OK,MessageBoxIcon.None, MessageBoxDefaultButton.Button1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            res = MessageBox.Show("Затейливое окно сообщения", "Заголовок окна сообщения", MessageBoxButtons.OKCancel,MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button2 );
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            res = MessageBox.Show("Я сейчас взорвусь как 300 тонн тротила...", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            res = MessageBox.Show("Не могу удалить файл!", "Ошибка", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button3);
+        }
+    }
+}
